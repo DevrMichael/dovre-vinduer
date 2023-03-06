fa66f77e0f9456af17a319e530dfaa90a76d1661
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-07 00:12:28 +0100

    Create door page

--- src/pages/Door.js
+++ src/pages/Door.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import doors from "../door.js";
 
 function Door() {
   return (
-    <div>Door</div>
-  )
+    <div>
+      <div className="door-intro-background">
+      <div className="door-intro-section">
+        <div className="door-intro-text">
+          <h1>Dører</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quam
+            reiciendis quo quaerat quas!
+          </p>
+        </div>
+        <div className="door-intro-image"></div>
+      </div>
+      </div>
+      <div className="door-grid">
+        {doors.map((door) => (
+          <img src={door.image}></img>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Door
+export default Door;
