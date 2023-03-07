1b0a87a385df8f7d1f87f3d50d41fab08e2ca163
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-07 01:04:19 +0100

    Create window page

--- src/pages/Window.js
+++ src/pages/Window.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import windows from "../window.js";
 
 function Window() {
   return (
-    <div>Window</div>
-  )
+    <div>
+      <div className="door-intro-background">
+        <div className="door-intro-section">
+          <div className="door-intro-text">
+            <h1>Vinduer</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quam
+              reiciendis quo quaerat quas!
+            </p>
+          </div>
+          <div className="window-intro-image"></div>
+        </div>
+      </div>
+      <div className="window-grid">
+        {windows.map((window) => (
+          <div>
+            <p>{window.title}</p>
+          <img src={window.image} />
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
 }
 
-export default Window
+export default Window;
