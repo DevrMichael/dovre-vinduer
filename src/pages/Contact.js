bb8aee65d67933be5b1845af12c27a1b3be84a93
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-07 23:25:38 +0100

    Add contact page form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-form-container'>
+      <h1>Contact Us</h1>
+      <form className='contact-form'>
+        <label htmlFor="name">Name</label>
+        <input type="text" />
+        <label htmlFor="email">Email</label>
+        <input type="email" />
+        <label htmlFor="textarea">Message</label>
+        <textarea name="textarea" id="" cols="30" rows="10"></textarea>
+        <button>Send Message</button>
+      </form>
+    </div>
   )
 }
 
