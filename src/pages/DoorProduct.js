35284778cf7b5144049780922dbc9df39fc0401b
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-07 22:40:16 +0100

    Style window and door product pages

--- src/pages/DoorProduct.js
+++ src/pages/DoorProduct.js
@@ -7,8 +7,42 @@ function DoorProduct() {
   const door = doors.find((door) => door.id === parseInt(doorId));
 
   return (
-    <div>
-      <img src={door.image} alt="" />
+    <div className="door-product-background">
+      <div className="door-product-container">
+        <div className="door-product-image">
+          <img src={door.image} alt="" />
+        </div>
+        <div className="door-product-info">
+          <div>
+            <h3>Door info 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis, totam?
+            </p>
+          </div>
+          <div>
+            <h3>Door info 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+              sit?
+            </p>
+          </div>
+          <div>
+            <h3>Door info 3</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              eaque!
+            </p>
+          </div>
+          <div>
+            <h3>Door info 4</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora,
+              illo.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/WindowProduct.js
+++ src/pages/WindowProduct.js
@@ -7,9 +7,43 @@ function DoorProduct() {
   const window = windows.find((door) => door.id === parseInt(windowId));
 
   return (
-    <div>
-      <img src={window.image} alt="" />
+    <div className="door-product-background">
+    <div className="door-product-container">
+      <div className="door-product-image">
+        <img src={window.image} alt="" />
+      </div>
+      <div className="door-product-info">
+        <div>
+          <h3>Door info 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis, totam?
+          </p>
+        </div>
+        <div>
+          <h3>Door info 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+            sit?
+          </p>
+        </div>
+        <div>
+          <h3>Door info 3</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+            eaque!
+          </p>
+        </div>
+        <div>
+          <h3>Door info 4</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora,
+            illo.
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
