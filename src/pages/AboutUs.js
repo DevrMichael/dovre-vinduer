0e5ebd0cf4da37ce3927826840a2389622dcec81
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-13 19:14:24 +0100

    Add about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div className="about-us__container">
+      <div className="about-us__intro-background">
+        <h1>About us</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="about-us__container-hero">
+        <div className="left-hero">
+          <h1>Our vision</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            saepe fuga quidem error quae ducimus eaque. Sint voluptatem libero
+            ullam.
+          </p>
+        </div>
+        <div className="about-us__right-hero"></div>
+      </div>
+      <div className="mini-hero-main">
+        <div className="left-mini-hero"></div>
+        <div className="right-mini-hero">
+          <h2>Our approach</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            placeat, qui quia voluptate aliquid sit laboriosam obcaecati aliquam
+            reprehenderit, illo alias dignissimos nihil temporibus! Error,
+            dolores! Unde quis magni perferendis voluptatem quos explicabo illum
+            quisquam?
+          </p>
+        </div>
+      </div>
+      <div className="about-us__container-hero">
+        <div className="left-hero">
+          <h1>Our process</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            saepe fuga quidem error quae ducimus eaque. Sint voluptatem libero
+            ullam.
+          </p>
+        </div>
+        <div className="right-hero"></div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
