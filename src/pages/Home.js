4dff6ca32402d82454f766bfaea0773f25baab48
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-03-17 18:31:14 +0100

    Update homepage text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 import { Slider } from "../slider";
@@ -6,124 +6,121 @@ import slides from "../door";
 import Windows from "../pages/Windows";
 
 function Home() {
-  let navigate = useNavigate(); 
-  const redirectGilje = () =>{ 
-    let path = `/omoss`; 
+  let navigate = useNavigate();
+  const redirectGilje = () => {
+    let path = `/omoss`;
     navigate(path);
-    window.scrollTo(0, 0)
-  }
-  const redirectVinduer = () =>{ 
-    let path = `/vinduer`; 
+    window.scrollTo(0, 0);
+  };
+  const redirectVinduer = () => {
+    let path = `/vinduer`;
     navigate(path);
-    window.scrollTo(0, 0)
-  }
-  const redirectDorer = () =>{ 
-    let path = `/dorer`; 
+    window.scrollTo(0, 0);
+  };
+  const redirectDorer = () => {
+    let path = `/dorer`;
     navigate(path);
-    window.scrollTo(0, 0)
-  }
+    window.scrollTo(0, 0);
+  };
   return (
     <div>
       <div className="hero">
         <div className="left-hero">
           <h1>Find the best fashion style for you</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
-            saepe fuga quidem error quae ducimus eaque. Sint voluptatem libero
-            ullam.
+          Riktig valg av dører og vinduer til hjemmet spiller en essensiell rolle i øking av trivsel, trygghet og velvære. Der kan vi hjelpe deg.
           </p>
           <button className="primaryButton">CTA Button</button>
         </div>
-        <div className="right-hero">
-        </div>
+        <div className="right-hero"></div>
       </div>
       <div className="features-background">
-      <div className="features">
-        <div className="top-features">
-          <h2>New Collection</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            optio, porro deleniti earum dolore asperiores?
-          </p>
+        <div className="features">
+          <div className="top-features">
+            <h2>Vi tilbyr</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem optio, porro deleniti earum dolore asperiores?
+            </p>
+          </div>
+          <div className="bottom-features">
+            <div>
+              <button className="secondaryButton" onClick={redirectVinduer}>
+                Vinduer
+              </button>
+            </div>
+            <div>
+              <button className="secondaryButton" onClick={redirectDorer}>
+                Dører
+              </button>
+            </div>
+            <div>
+              <button className="secondaryButton" onClick={redirectGilje}>
+                Gilje Sense
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="bottom-features">
-          <div>
-            <button className="secondaryButton" onClick={redirectVinduer}>Vinduer</button>
-          </div>
-          <div>
-            <button className="secondaryButton" onClick={redirectDorer}>Dører</button>
-          </div>
-          <div>
-            <button className="secondaryButton" onClick={redirectGilje}>Gilje Sense</button>
-          </div>
-        </div>
-      </div>
       </div>
       <div className="mini-hero">
         <div className="mini-hero-main">
           <div className="left-mini-hero"></div>
           <div className="right-mini-hero">
-            <h2>Best fashion since 2014</h2>
+            <h2>Hvem er vi?</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-              placeat, qui quia voluptate aliquid sit laboriosam obcaecati
-              aliquam reprehenderit, illo alias dignissimos nihil temporibus!
-              Error, dolores! Unde quis magni perferendis voluptatem quos
-              explicabo illum quisquam?
+              Med over 20 års erfaring i bransjen hjelper vi deg med å skreddersy løsninger
+              tilpasset deg, og dine prosjekters behov. Vi tilbyr et bredt utvalg av vinduer og dører som passer for alle og enhver.
             </p>
           </div>
         </div>
         <div className="mini-hero-overflow-div">
           <div>
-            <h3>2014</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <h3>30+</h3>
+            <p>års erfaring i bransjen</p>
           </div>
           <div>
-            <h3>8900+</h3>
-            <p>Lorem ipsum dolor sit.</p>
+            <h3>6</h3>
+            <p>vindustyper for alle fasader</p>
           </div>
           <div>
-            <h3>3103+</h3>
-            <p>Lorem ipsum dolor sit.</p>
+            <h3>50+</h3>
+            <p>unike dører</p>
           </div>
         </div>
       </div>
       <div className="slider-container">
-        <h2>Our Doors</h2>
+        <h2>Dører</h2>
         <Slider slides={slides} />
       </div>
       <div className="window-container-header">
-        <h2>Our Windows</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptas
-          obcaecati cum doloremque quidem quibusdam.
+        Visste du at du kan spare stort på strømregningen KUN ved å bytte ut enkelte av dine vinduer?
         </p>
       </div>
-        <Windows />
-        <div className="showroom-container">
-          <div className="hero">
-            <div className="left-showroom">
-              <h1>Our Showroom</h1>
-              <p>Our showroom is centrally located in Lørenskog.</p>
-            </div>
-            <div className="right-showroom">
-              <div className="homepage-map-container">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.9098922821365!2d10.72518285207197!3d59.91704278177933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416dd779ba357b%3A0x677038c9acc2591c!2sDet%20kongelige%20slott!5e0!3m2!1sno!2sno!4v1677587198537!5m2!1sno!2sno"
-                  title="map"
-                  width="600"
-                  height="450"
-                  allowFullScreen=""
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </div>
+      <Windows />
+      <div className="showroom-container">
+        <div className="hero">
+          <div className="left-showroom">
+            <h1>Vårt Showroom</h1>
+            <p>Vårt showroom er lokalisert sentralt i Lørenskog.</p>
+          </div>
+          <div className="right-showroom">
+            <div className="homepage-map-container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.9098922821365!2d10.72518285207197!3d59.91704278177933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416dd779ba357b%3A0x677038c9acc2591c!2sDet%20kongelige%20slott!5e0!3m2!1sno!2sno!4v1677587198537!5m2!1sno!2sno"
+                title="map"
+                width="600"
+                height="450"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
