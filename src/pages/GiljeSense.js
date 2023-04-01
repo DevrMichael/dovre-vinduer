d2ef887ab1fe065f4560b9cb8466bbd419dd31c3
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-04-01 12:41:05 +0200

    Add contact text on Gilje Sense

--- src/pages/GiljeSense.js
+++ src/pages/GiljeSense.js
@@ -18,7 +18,9 @@ function GiljeSense() {
         <div className="left-hero">
           <h1>Hva er Gilje Sense?</h1>
           <p>Vi tilbyr Gilje Sense som gjør at du har full kontroll over dine dører og vinduer fra en enkel app på mobiltelefonen din. Appen sjekker om alle dine dører og vinduer er lukket og låst. <br /> <br />
-        Gilje Sense gir deg en indre rohet om at hjemmet ditt er trygt når du er på butikken, jobb eller på reise.</p>
+        Gilje Sense gir deg en indre rohet om at hjemmet ditt er trygt når du er på butikken, jobb eller på reise. <br /> <br />
+        Kontakt oss for mer utfyllende informasjon.</p>
+        
         </div>
         <div className="gilje-sense__container__right-hero"><iframe title="vimeo-player" src="https://player.vimeo.com/video/238541686?h=feab716ecf" width="640" height="360" frameborder="0"    allowfullscreen></iframe></div>
       </div>
