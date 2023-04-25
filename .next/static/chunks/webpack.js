79096f2a85527556dedebd37909981d602b0e08d
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-04-25 17:09:21 +0200

    Add build files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4565d239bda8485e"; }
+/******/ 		__webpack_require__.h = function() { return "1330ac60e53b43cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
