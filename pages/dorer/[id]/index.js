f8fb04171772e46c8a09be6b33085bdc39e4bc62
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-05-06 03:16:51 +0200

    Add metadata to product pages

--- pages/dorer/[id]/index.js
+++ pages/dorer/[id]/index.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import doors from "../../../data/door";
-import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import doors from '../../../data/door';
+import Image from 'next/image';
+import Head from 'next/head';
 
 function DoorProduct() {
   const router = useRouter();
   const { id } = router.query;
 
-const [title, setTitle] = useState("")
-const [imageSrc, setImageSrc] = useState("")
+  const [title, setTitle] = useState('');
+  const [imageSrc, setImageSrc] = useState('');
 
-useEffect(() => {
+  useEffect(() => {
     if (!id) return;
     const door = doors.find((door) => door.id === id);
     setTitle(door.title);
@@ -19,35 +20,41 @@ useEffect(() => {
 
   return (
     <div className="door-product-background">
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Dører fra Gilje"
+          key="desc"
+        />
+      </Head>
       <div className="door-product-container">
         <div className="door-product-image">
           <h2>{title}</h2>
-          <Image src={imageSrc} alt="gilje dører" width={100} height={350}/>
+          <Image src={imageSrc} alt="gilje dører" width={100} height={350} />
         </div>
         <div className="door-product-info">
           <div>
             <h3>Kvalitet</h3>
             <p>
-              Giljes ytterdører er produsert for å tåle et skiftende norsk klima, og ikke minst hard bruk.
+              Giljes ytterdører er produsert for å tåle et skiftende norsk
+              klima, og ikke minst hard bruk.
             </p>
           </div>
           <div>
             <h3>Kompaktlaminat</h3>
-            <p>
-              Kompaktlaminat er standard på alle våre ytterdører.
-            </p>
+            <p>Kompaktlaminat er standard på alle våre ytterdører.</p>
           </div>
           <div>
             <h3>Bredde</h3>
             <p>
-              Våre dører kan leveres inntil 129 cm bredde for enkeltdører og 239 cm bredde for to-fløyede dører.
+              Våre dører kan leveres inntil 129 cm bredde for enkeltdører og 239
+              cm bredde for to-fløyede dører.
             </p>
           </div>
           <div>
             <h3>Høyde</h3>
-            <p>
-              Høyde kan leveres inntil 239 cm.
-            </p>
+            <p>Høyde kan leveres inntil 239 cm.</p>
           </div>
         </div>
       </div>
--- pages/vinduer/[id]/index.js
+++ pages/vinduer/[id]/index.js
@@ -1,45 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import windows from "../../../data/window";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import windows from '../../../data/window';
 import Image from 'next/image';
+import Head from 'next/head';
 
 function WindowProduct() {
-    const router = useRouter();
+  const router = useRouter();
   const { id } = router.query;
 
-const [title, setTitle] = useState("")
-const [imageSrc, setImageSrc] = useState("")
-const [content, setContent] = useState("")
-const [pageContent, setPageContent] = useState("")
+  const [title, setTitle] = useState('');
+  const [imageSrc, setImageSrc] = useState('');
+  const [content, setContent] = useState('');
+  const [pageContent, setPageContent] = useState('');
 
-useEffect(() => {
+  useEffect(() => {
     if (!id) return;
     const window = windows.find((window) => window.id === id);
     setTitle(window.title);
     setImageSrc(window.image.src);
-    setContent(window.content)
-    setPageContent(window["page-content"])
+    setContent(window.content);
+    setPageContent(window['page-content']);
   }, [id]);
 
   return (
     <div>
-    <div className="window-product-container">
-      <div className="window-product-image">
-        <Image src={imageSrc} alt="gilje vinduer" width={300} height={300}/>
-      </div>
-      <div className="window-product-info">
-        <div>
-          <h3>{title}</h3>
-          <br />
-          <p>{content}</p>
-          <br />
-          <p>
-            {pageContent}
-          </p>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Vinduer fra Gilje "
+          key="desc"
+        />
+      </Head>
+      <div className="window-product-container">
+        <div className="window-product-image">
+          <Image src={imageSrc} alt="gilje vinduer" width={300} height={300} />
+        </div>
+        <div className="window-product-info">
+          <div>
+            <h3>{title}</h3>
+            <br />
+            <p>{content}</p>
+            <br />
+            <p>{pageContent}</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
