a7642f4f61094ff4e8a99e5cd377ea78d08ad51e
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-05-30 00:43:39 +0200

    Format phone text

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -18,8 +18,8 @@ function Contact() {
         <p>
           Ta kontakt med oss i dag for mer utfyllende informasjon om våre
           produkter og tjenester. <br /> <br /> Send oss en mail ved å klikke{' '}
-          <a href="mailto:naeem@dovrevinduer.no">her</a> eller ring oss på tlf{' '}
-          <a href="tel:004790761578">+47 90761578</a>
+          <a href="mailto:naeem@dovrevinduer.no">her</a> eller ring oss på tlf.{' '}
+          <a href="tel:004790761578">90761578</a>
         </p>
         {/* <form className='contact-form'>
         <label htmlFor="name">Name</label>
