1b6ba02c616876f854abf12128a94df670c065e8
Michael Siddiqi <bismics@gmail.com>
2025-01-23 11:20:13 +0100

    add es lint file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    // Add any specific ESLint rules you want here
+  },
+};
